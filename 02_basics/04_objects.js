3fa11fb3256f82de283d918a9c0bec21eb643e65
DKSingh1604 <dev1604karan@gmail.com>
2024-08-31 22:41:14 +0530

    objects learnt

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,2 +1,66 @@
 //Singleton
-const tinderUser = new Object();
+// const tinderUser = new Object();// is a singleton object
+const tinderUser = {}// not a singleton object
+//above both are same
+
+tinderUser.id = "123abc";
+tinderUser.name = "Sammy";
+tinderUser.isLoggedIn = false;
+
+console.log(tinderUser);
+
+const regularUser = {
+    email: "someone@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Dev",
+            middlename: "Karan",
+            lastname: "Singh"
+
+        }
+    }
+}
+
+console.log(regularUser.fullname?.userfullname.lastname);
+// ? -> is put to apply condition if available do it else not
+
+const obj1 ={
+    1: "a",
+    2: "b"
+}
+const obj2 = {
+    3: "c",
+    4: "d"
+}
+const obj3 = {
+    5: "e",
+    6: "f"
+}
+// TO MERGE TO OBJECTS
+// const obj3 = {obj1, obj2};
+// const obj4 = Object.assign({}, obj1, obj2, obj3);
+// console.log(obj4);
+
+//MERGING USING SPREAD FUNCTION
+const obj4 = {...obj1, ...obj2, ...obj3};
+// console.log(obj4);
+
+//OBJECTS INSIDE ARRAY  
+const users = [
+    {//obj1
+        email: "devkainf@gmail.com",
+        id: 123
+    },
+    {//obj2
+
+    }
+]
+// console.log(users[0].email);
+// console.log(users[0].id);
+
+console.log(Object.keys(tinderUser));//to get all keys of tinderUser in array format
+console.log(Object.values(tinderUser));//to get all values of tinderUser in array format
+console.log(Object.entries(tinderUser));//to get all keys and corresponding values on array format
+
+//to know if some property is the class or not
+console.log(tinderUser.hasOwnProperty('isLogged'));
